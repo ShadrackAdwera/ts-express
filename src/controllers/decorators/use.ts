fec02938cfcddcf6993ff92529bbce7d6326e7c7
ShadrackAdwera <adweshshaddie@gmail.com>
2022-05-11 08:49:39 +0300

    wire up use decorator to handle middleware functions

--- src/controllers/decorators/use.ts
+++ src/controllers/decorators/use.ts
@@ -3,7 +3,7 @@ import { RequestHandler } from 'express';
 import { MetaDataKeys } from './Metadata';
 
 export function use(middleware: RequestHandler) {
-  return function (target: Function, key: string, desc: PropertyDescriptor) {
+  return function (target: any, key: string, desc: PropertyDescriptor) {
     const middlewares =
       Reflect.getMetadata(MetaDataKeys.Middleware, target, key) || [];
 
